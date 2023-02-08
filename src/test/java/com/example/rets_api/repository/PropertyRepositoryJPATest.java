8552a879d819b8dff3a9f0d57f56a72f9454a4da
taise.marques <taise.marques@hotmail.com>
2023-02-08 12:34:11 -0500

    changing checkAllBasicFieldsFromProperty to private, changing assertions

--- src/test/java/com/example/rets_api/repository/PropertyRepositoryJPATest.java
+++ src/test/java/com/example/rets_api/repository/PropertyRepositoryJPATest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import static com.example.rets_api.repository.UtilsTest.createRoom;
 import static org.assertj.core.api.Assertions.assertThat;
 import static com.example.rets_api.entity.Enums.RoomType;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -32,7 +33,7 @@ public class PropertyRepositoryJPATest {
 
     @Test
     public void should_store_a_Property() {
-        //Creating Property
+        //Creating
         PropertyEntity propertyToSave = UtilsTest.createPropertyEntityWithBasicFields();
 
         //Saving
@@ -44,38 +45,32 @@ public class PropertyRepositoryJPATest {
 
     @Test
     public void should_store_a_PropertyAndSchoolAndRoom() {
-        //Creating property
+        //Creating
         PropertyEntity propertyToSave = UtilsTest.createPropertyEntityWithBasicFields();
-
-        //Setting school
         propertyToSave.setSchoolList(Arrays.asList(UtilsTest.createSchool("primarySchool", "jrHighSchool")));
-
-        //Setting rooms
         propertyToSave.setRoomList(Arrays.asList(createRoom(RoomType.LIVING_ROOM), createRoom(RoomType.MAIN_FLOOR_BEDROOM)));
         propertyToSave.setBedroomsQty(1);
 
         //Saving
         PropertyEntity propertySaved = propertyRepository.saveAndFlush(propertyToSave);
 
-        //Validating Property basic fields
+        //Validating
         checkAllBasicFieldsFromProperty(propertySaved);
 
-        //Validating School
-        assertThat(propertySaved.getSchoolList()).isNotNull();
-        assertThat(propertySaved.getSchoolList().size() == propertyToSave.getSchoolList().size());
-        assertThat(propertySaved.getSchoolList().get(0).getPrimarySchool() == propertyToSave.getSchoolList().get(0).getPrimarySchool());
-        assertThat(propertySaved.getSchoolList().get(0).getJrHigh() == propertyToSave.getSchoolList().get(0).getPrimarySchool());
+        assertNotNull(propertySaved.getSchoolList());
+        assertEquals(propertySaved.getSchoolList().size(), propertyToSave.getSchoolList().size());
+        assertEquals(propertySaved.getSchoolList().get(0).getPrimarySchool(), propertyToSave.getSchoolList().get(0).getPrimarySchool());
+        assertEquals(propertySaved.getSchoolList().get(0).getJrHigh(), propertyToSave.getSchoolList().get(0).getJrHigh());
 
-        //Validating Rooms
-        assertThat(propertySaved.getRoomList()).isNotNull();
-        assertThat(propertySaved.getRoomList().size() == propertyToSave.getRoomList().size());
-        assertThat(propertySaved.getRoomList().get(0).getRoomType() == propertyToSave.getRoomList().get(0).getRoomType());
-        assertThat(propertySaved.getRoomList().get(1).getRoomType() == propertyToSave.getRoomList().get(1).getRoomType());
+        assertNotNull(propertySaved.getRoomList());
+        assertEquals(propertySaved.getRoomList().size(), propertyToSave.getRoomList().size());
+        assertEquals(propertySaved.getRoomList().get(0).getRoomType(), propertyToSave.getRoomList().get(0).getRoomType());
+        assertEquals(propertySaved.getRoomList().get(1).getRoomType(), propertyToSave.getRoomList().get(1).getRoomType());
 
     }
 
-    public void checkAllBasicFieldsFromProperty(PropertyEntity property){
-        assertThat(property.getPropertyId()).isNotNull();
+    private void checkAllBasicFieldsFromProperty(PropertyEntity property){
+        assertNotNull(property.getPropertyId());
         assertThat(property).hasFieldOrPropertyWithValue("age", property.getAge());
         assertThat(property).hasFieldOrPropertyWithValue("horseFacilities", property.getHorseFacilities());
         assertThat(property).hasFieldOrPropertyWithValue("horseFacilitiesIndicator", property.getHorseFacilitiesIndicator());
