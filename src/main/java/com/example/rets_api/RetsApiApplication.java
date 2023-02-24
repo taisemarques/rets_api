affdcbe57245e1541cb09f006dc0bf9d81f6457f
taise.marques <taise.marques@hotmail.com>
2023-02-24 16:50:12 -0500

    adding swagger doc

--- src/main/java/com/example/rets_api/RetsApiApplication.java
+++ src/main/java/com/example/rets_api/RetsApiApplication.java
@@ -2,9 +2,36 @@ package com.example.rets_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class RetsApiApplication {
+
+	@Bean
+	public Docket docket(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.groupName("rets-api")
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.rets_api.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Rets Api")
+				.description("Rets reference application")
+				.version("1.0")
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetsApiApplication.class, args);
